1f05c0d5f6b520d2a4242331ca1de08c162b13d7
christofranz <franzke.christoph@web.de>
2020-12-29 11:10:21 +0100

    Add docstrings to common.py and remove dist func from search

--- filtering/common.py
+++ filtering/common.py
@@ -42,7 +42,15 @@ def get_books_liked_by_user(user_id, user_book_rating, min_rating):
 
 
 def find_in_string(sub, string):
-    """Check if a substring is contained in another string.    """
+    """Check if a substring is contained in another string.
+    
+    :param sub: Substring
+    :type: str
+    :param string: String which is checked if the substring is contained
+    :type: str
+    :return: Result if the substring is contained in the string
+    :rtype: boolean
+    """
     sub_l = sub.lower()
     string_l = string.lower()
     if sub_l in string_l or string_l in sub_l:
@@ -51,8 +59,17 @@ def find_in_string(sub, string):
         return 0
 
 
-def find_book_ids(books, author=None, title=None, dist_thresh=0.7):
-    """TODO"""
+def find_book_ids(books, author=None, title=None):
+    """Find books by author and/or title in the dataset.
+    
+    :param books: Dataframe containing the meta data
+    :type: pandas dataframe
+    :param author: Author to look up
+    :type: str
+    :param title: Title to look up
+    :type: str
+    :param dist_th
+    """
     if not author and not title:
         print("Please add an author and/or a title.")
     else:
@@ -62,8 +79,7 @@ def find_book_ids(books, author=None, title=None, dist_thresh=0.7):
         if author is not None:
             # find all books with this author
             for idx, a in enumerate(all_authors):
-                dist_a = find_in_string(author, a) #TODO: alternative distance function
-                if dist_a >= dist_thresh:
+                if find_in_string(author, a):
                     book_candidates.append(books.iloc[idx]["book_id"])
 
             # iterate over books from author
@@ -73,16 +89,14 @@ def find_book_ids(books, author=None, title=None, dist_thresh=0.7):
                 titles_from_author = []
                 for book_id in book_candidates:
                     t = books.loc[books["book_id"]==book_id].squeeze().title
-                    dist_t = find_in_string(t, title)
-                    if dist_t < dist_thresh:
+                    if not find_in_string(t, title):
                         relevant_book_ids.remove(book_id)
 
         else:
             # if author is not given, go only through titles
             relevant_book_ids = []
             for idx, t in enumerate(all_titles):
-                dist_t = find_in_string(title, t)
-                if dist_t >= dist_thresh:
+                if find_in_string(title, t):
                     relevant_book_ids.append(books.iloc[idx]["book_id"])
 
         return relevant_book_ids
